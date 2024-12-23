4c86fc507ced4f2e9b84ff1e935db7217aa10702
DronskiDron <andronaf93@gmail.com>
2024-12-23 15:30:14 +0300

    Experiment 1.

--- Assets/_TestProj/Scripts/TestMonobeh.cs
+++ Assets/_TestProj/Scripts/TestMonobeh.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class TestMonobeh : MonoBehaviour
 {
-    [SerializeField] private string _testField;
+    [SerializeField] private int _testField;
     [SerializeField] private int _newTestField;
     [SerializeField] private string _oneMoreTestField;
     [SerializeField] private bool _boolTestField;
