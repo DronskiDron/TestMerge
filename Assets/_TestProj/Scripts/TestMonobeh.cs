aca5faaa4752eab63223d13125b316b57371bed7
DronskiDron <andronaf93@gmail.com>
2024-12-23 15:28:29 +0300

    Experiment 0.

--- Assets/_TestProj/Scripts/TestMonobeh.cs
+++ Assets/_TestProj/Scripts/TestMonobeh.cs
@@ -5,4 +5,5 @@ public class TestMonobeh : MonoBehaviour
     [SerializeField] private string _testField;
     [SerializeField] private int _newTestField;
     [SerializeField] private string _oneMoreTestField;
+    [SerializeField] private bool _boolTestField;
 }
